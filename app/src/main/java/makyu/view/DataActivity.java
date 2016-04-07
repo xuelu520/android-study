aecc21bb37a7cf0cffa128fc6e76dfecbf92685a
xuelu <289415287@qq.com>
2016-04-07 11:29:28 +0800

    添加SharePreference

--- app/src/main/java/makyu/view/DataActivity.java
+++ app/src/main/java/makyu/view/DataActivity.java
@@ -1,15 +1,17 @@
 package makyu.view;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class DataActivity extends AppCompatActivity {
-
-    public Button addButton;
-    public Button getButton;
+public class DataActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,29 +20,37 @@ public class DataActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        addButton = (Button) findViewById(R.id.addPerferenceData);
-        addButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                addPerferenceData();
-            }
-        });
-
-        getButton = (Button) findViewById(R.id.getPerferenceData);
-        getButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-              String data = getPerferenceData();
-            }
-        });
+        et = (EditText) findViewById(R.id.et);
+        Button bt = (Button) findViewById(R.id.bt);
+        Button bt2 = (Button) findViewById(R.id.bt2);
+        bt.setOnClickListener(this);
+        bt2.setOnClickListener(this);
     }
 
-    private void addPerferenceData() {
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt:
+                String etString = et.getText().toString();
+                saveData(this,etString);
+                break;
+            case R.id.bt2:
+                loadData(this);
+                break;
+            default:
+                break;
+        }
 
     }
-
-    private String getPerferenceData() {
-        return "";
+    private void loadData(Context context) {
+        SharedPreferences sp = context.getSharedPreferences("config", MODE_PRIVATE);
+        Toast.makeText(this, sp.getString("content", "").toString(), Toast.LENGTH_SHORT).show();
+    }
+    private void saveData(Context context,String string){
+        SharedPreferences sp = context.getSharedPreferences("config", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString("content", string);
+        editor.apply();
     }
 
 }
