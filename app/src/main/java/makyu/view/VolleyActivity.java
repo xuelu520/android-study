3fabd0bdef415282448d3f35e606113ec69b1efb
xuelu <289415287@qq.com>
2016-05-16 18:17:11 +0800

    Volley 加载图片

--- app/src/main/java/makyu/view/VolleyActivity.java
+++ app/src/main/java/makyu/view/VolleyActivity.java
@@ -1,5 +1,6 @@
 package makyu.view;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,11 +9,13 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
@@ -23,7 +26,9 @@ import makyu.view.util.VolleyController;
 
 public class VolleyActivity extends AppCompatActivity implements View.OnClickListener{
     Button getRequestButton;
+    Button getImgButton;
     TextView resultTv;
+    ImageView imgView;
     RequestQueue mQueue;
 
 
@@ -42,7 +47,11 @@ public class VolleyActivity extends AppCompatActivity implements View.OnClickLis
         mQueue = Volley.newRequestQueue(getApplicationContext());
         getRequestButton = (Button) findViewById(R.id.getRequest);
         getRequestButton.setOnClickListener(this);
+
+        getImgButton = (Button) findViewById(R.id.getImage);
+        getImgButton.setOnClickListener(this);
         resultTv = (TextView) findViewById(R.id.result);
+        imgView = (ImageView) findViewById(R.id.img);
     }
 
     @Override
@@ -51,7 +60,28 @@ public class VolleyActivity extends AppCompatActivity implements View.OnClickLis
             case R.id.getRequest :
                 getRequest();
                 break;
+            case R.id.getImage :
+                getImg("http://pic2.178.com/58/580446/month_1104/4351c091b76a44fb93257bfbda623ef0.jpg", imgView);
+                break;
         }
+    }
+
+    private void getImg(String imgUrl, final ImageView imgView) {
+        ImageRequest imageRequest = new ImageRequest(
+                imgUrl,
+                new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap response) {
+                        imgView.setImageBitmap(response);
+                    }
+                }, 0, 0, Bitmap.Config.RGB_565, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("VolleyTag", error.getMessage());
+                imgView.setImageResource(R.drawable.ic_phone_black_18dp);
+            }
+        });
+        mQueue.add(imageRequest);
     }
 
     private void getRequest() {
